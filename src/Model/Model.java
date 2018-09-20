7ca5def2223d2800d55bf4a3e9c5840a025fe2c2
Shepherd <kshe5782@740s-pl-112.local>
2018-09-20 09:36:42 -0600

    Made setter, and added values to variables

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,5 +1,57 @@
 package Model;
 
-public class Model {
+//Import section
 
+
+//data member section
+//Default values:
+// All numbers => 0 
+//boolean => false
+//objects => null!!!! 
+public class Model 
+{
+private String name;
+private boolean hasStrap;
+private double strapsNumber;
+private int location;
+{
+
+
+/**
+ * Default constructor for after customization
+ * AKA I don't know info yet 
+ * Initializing data members with "bad" values to e changed
+ */
+public Model()
+{
+}
+{
+
+
+this.name = "50 Cent";
+this.hasStrap = true; 
+this.strapsNumber = -100;
+this.location = -801;
+}
+
+/**
+ * Better constructor (I know the info)
+ * @param 
+ */
+
+public setName(String name)
+{
+	this.name  = name;
+}
+public void sethasStrap(boolean hasStrap)
+{
+	this.hasStrap = hasStrap;
+}
+public void setstrapsNumber (double strapsNumber)
+{
+	this.strapsNumber = strapsNumber; 
+}
+public setlocation(int location)
+{
+	this.location = location;
 }
