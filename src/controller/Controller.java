64ed3ae369f4e38c5f11280ba13058a8bd98abab
Shepherd <kshe5782@740s-pl-112.local>
2018-09-27 09:31:06 -0600

    debugged, and messed with more while loops

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,48 +3,49 @@ package controller;
 import Model.Model;
 import javax.swing.JOptionPane;
 
-public class Controller 
+public class Controller
 {
-{
-}
-	//slash star star starts java doc
+	// slash star star starts java doc
 	/**
 	 * Builds instance of Controller
 	 */
 	public Controller()
 	{
 
-}
+	}
+
 	/**
 	 * Program starts calling methods to run program
 	 */
 	public void start()
-{
+	{
 		loopy();
-}
-	
+	}
+
 	private void loopy()
 	{
 
-		//define the variable before the loop
+		// define the variable before the loop
 		boolean isDone = false;
 		int count = 0;
-		
-		while (!isDone);  //Test the variable
+
+		while (!isDone)
+			 // Test the variable
+		{
+			JOptionPane.showMessageDialog(null, "blah blah blah");
+			// Eventually change the loop variable
+			count++;
+			if (count >= 10)
+			{
+				isDone = true; // update variable
 			}
-	JOptionPane.showMessageDialog(null, "I am basically Monky");
-	//Eventually change the loop variable 
-	count++;
-	if(count >= 10)
-	{
-		isDone = true; //update variable
+		}
+
+		for (int loop = 0; loop < 30; loop += 2)
+		{
+			JOptionPane.showMessageDialog(null, "The loop value is : " + loop);
+		}
+
+		// private void askUser
 	}
-	}
-{
-	for (int loop = 0; loop < 30; loop += 2 )
-{
-	JOptionPane.showMessageDialog(null, "The loop value is : " + loop);
-			}
-	
-	
-}}
+}
